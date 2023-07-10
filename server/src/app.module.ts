9ba0300ef5c864332e85d759056d14a26675c9c1
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-07-10 16:05:34 +0600

    Tags resource created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,6 +7,7 @@ import { SettingsModule } from './settings/settings.module';
 import { TypesModule } from './types/types.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AttributesModule } from './attributes/attributes.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { AttributesModule } from './attributes/attributes.module';
     TypesModule,
     CategoriesModule,
     AttributesModule,
+    TagsModule,
   ],
   controllers: [],
   providers: [],
